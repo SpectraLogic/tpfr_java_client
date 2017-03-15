c6af7f39a4ed959000a6709a6127ad3ee01cdc64
Sharon Shabtai <sharons@spectralogic.com>
2017-03-14 21:05:49 -0600

    improve ReadFileHelper

--- client/src/test/java/com/spectralogic/tpfr/client/ReadFileHelper.java
+++ client/src/test/java/com/spectralogic/tpfr/client/ReadFileHelper.java
@@ -1,19 +1,23 @@
 package com.spectralogic.tpfr.client;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class ReadFileHelper {
-    public static String readFile(final String fileName){
+final class ReadFileHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientImpl.class);
+
+    static String readFile(final String fileName){
 
         String result = "";
 
         final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         try {
             result = IOUtils.toString(classLoader.getResourceAsStream(fileName));
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (final IOException e) {
+            LOG.error("Failed to read xml from resource", e);
         }
 
         return result;
