f0249098022ebb91413a17b917ae4cd734fd7d9f
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2023-04-25 10:38:31 -0600

    Update copyright years

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 /*
 * ******************************************************************************
-*   Copyright 2002 Spectra Logic Corporation. All Rights Reserved.
+*   Copyright 2002-2023 Spectra Logic Corporation. All Rights Reserved.
 *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
 *   this file except in compliance with the License. A copy of the License is located at
 *
